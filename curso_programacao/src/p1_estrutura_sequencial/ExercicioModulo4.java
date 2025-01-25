9c54c89be553198c1df682d06e231b616f5cb62c
Nicolas Figueiredo <nicolasgregoriof@gmail.com>
2025-01-25 12:37:23 -0300

    teste

--- curso_programacao/src/p1_estrutura_sequencial/ExercicioModulo4.java
+++ curso_programacao/src/p1_estrutura_sequencial/ExercicioModulo4.java
@@ -9,7 +9,7 @@ public class ExercicioModulo4 {
 		/* Exercício 4 - Fazer um programa que leia o número de um funcionário, 
 		 * seu número de horas trabalhadas, o valor que recebe por
 		 * hora e calcula o salário desse funcionário. A seguir, mostre o 
-         * número e o salário do funcionário, com duas casas decimais..*/
+         * número e o salário do funcionário, com duas casas decimais.. */
 		
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
