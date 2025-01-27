9d8fca983d52ebd658bf20284bcc2df0c0c08fc0
Nicolas Figueiredo <nicolasgregoriof@gmail.com>
2025-01-27 16:16:56 -0300

    Estrutura condicional

--- curso_programacao/src/p1_estrutura_sequencial/ExercicioModulo1.java
+++ curso_programacao/src/p1_estrutura_sequencial/ExercicioModulo1.java
@@ -5,18 +5,19 @@ import java.util.Scanner;
 public class ExercicioModulo1 {
 
 	public static void main(String[] args) {
-		/* Exercício 1 - Faça um programa para ler dois valores inteiros, e depois mostrar na tela a soma desses números com uma
-		mensagem explicativa, conforme exemplos.*/
+		/*
+		 * Exercício 1 - Faça um programa para ler dois valores inteiros, e depois
+		 * mostrar na tela a soma desses números com uma mensagem explicativa, conforme
+		 * exemplos.
+		 */
 		Scanner sc = new Scanner(System.in);
 		int num1, num2, resultado;
 		num1 = sc.nextInt();
-		num2 = sc.nextInt();	
+		num2 = sc.nextInt();
 		resultado = num1 + num2;
-		System.out.printf("SOMA = %d%n",resultado);
+		System.out.printf("SOMA = %d%n", resultado);
 		sc.close();
-		
-		
-		
+
 	}
 
 }
---
+++ curso_programacao/src/p3_estrutura_repetitiva/Exerc1.java
@@ -0,0 +1,30 @@
+package p3_estrutura_repetitiva;
+
+import java.util.Scanner;
+import java.util.Locale;
+
+public class Exerc1 {
+
+	public static void main(String[] args) {
+		// Escreva um programa que repita a leitura de uma senha até que ela seja válida. 
+		// Para cada leitura de senha incorreta informada, escrever a mensagem
+		// "Senha Invalida". Quando a senha for informada corretamente deve ser
+		// impressa a mensagem "Acesso Permitido" e o algoritmo encerrado. 
+		// Considere que a senha correta é o valor 2002.
+		
+		
+		Scanner sc = new Scanner(System.in);
+		int senha = sc.nextInt();
+		
+		while (senha != 2002) {
+			System.out.println("Senha Inválida");
+			senha = sc.nextInt();
+		}
+		
+		System.out.println("Acesso Permitido");
+		
+		sc.close();
+
+	}
+
+}
