b58e44fbcf7e49d600e8a73683a07c0510c852cc
Nicolas Figueiredo <nicolasgregoriof@gmail.com>
2025-02-22 22:46:28 -0300

    implements

---
+++ 02_desafios/pj_interface/src/model/entities/Installment.java
@@ -0,0 +1,43 @@
+package model.entities;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class Installment {
+	private LocalDate dueDate;
+	private Double amount;
+	
+	
+	public Installment(LocalDate dueDate, Double amount) {
+		this.dueDate = dueDate;
+		this.amount = amount;
+	}
+
+	public LocalDate getDueDate() {
+		return dueDate;
+	}
+
+	public void setDueDate(LocalDate dueDate) {
+		this.dueDate = dueDate;
+	}
+
+	public Double getAmount() {
+		return amount;
+	}
+
+	public void setAmount(Double amount) {
+		this.amount = amount;
+	}
+	
+	public String toString() {
+	    DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+	    StringBuilder sb = new StringBuilder();
+	    
+	    sb.append("Parcelas:\n");
+	    sb.append(getDueDate().format(fmt) + " - " + String.format("%.2f", getAmount()) + "\n");
+	    
+	    return sb.toString();
+	}
+	
+	
+}
